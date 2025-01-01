28d6dc2a3f7d793551cef911a59d0e2c8522e370
Margareta Valencia <margaretavalencia16@gmail.com>
2025-01-01 22:34:27 +0700

    done

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from drowsiness_detection import drowsiness_detection_page
 from about import about_page
 
 # Streamlit UI - Page Navigation
@@ -9,9 +8,7 @@ def main():
     # Sidebar Navigation
     page = st.sidebar.selectbox("Select a Page", ["Drowsiness Detection", "About"])
 
-    if page == "Drowsiness Detection":
-        drowsiness_detection_page()
-    elif page == "About":
+    if page == "About":
         about_page()
 
 if __name__ == "__main__":
