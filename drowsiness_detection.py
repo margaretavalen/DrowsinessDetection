8bdda3b7e07c37fa8626e66c10285a714eb36568
Margareta Valencia <margaretavalencia16@gmail.com>
2025-01-04 16:12:49 +0700

    done

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -121,7 +121,10 @@ class DrowsinessDetectionPage:
     def process_webcam(self):
         FRAME_WINDOW = st.image([])
         run = st.checkbox("Start Detection")
-        
+
+        if not run:
+            return
+
         cap = cv2.VideoCapture(cv2.CAP_V4L2)
         if not cap.isOpened():
             st.error("Unable to access the webcam. Try changing the camera index.")
@@ -149,10 +152,6 @@ class DrowsinessDetectionPage:
         finally:
             cap.release()
             FRAME_WINDOW.image([])
-
-        if not run:
-            return
-
 
     def process_image(self):
         uploaded_image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
